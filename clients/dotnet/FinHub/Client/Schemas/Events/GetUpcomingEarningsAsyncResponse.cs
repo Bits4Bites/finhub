62cd5a644b3456a0d1cd4b3be7e4bae0c7e70241
Thanh Nguyen <btnguyen2k@gmail.com>
2026-09-21 14:05:44 +1000

    Update dotnet client

--- clients/dotnet/FinHub/Client/Schemas/Events/GetUpcomingEarningsAsyncResponse.cs
+++ clients/dotnet/FinHub/Client/Schemas/Events/GetUpcomingEarningsAsyncResponse.cs
@@ -1,6 +1,12 @@
 using FinHub.Client.Models.Events;
-using FinHub.Client.Schemas;
+using MyPo.Shared.Api;
 
 namespace FinHub.Client.Schemas.Events;
 
-public sealed class GetUpcomingEarningsAsyncResponse : AsyncApiResponse<IReadOnlyList<UpcomingEarningsEvent>>;
+public sealed class GetUpcomingEarningsAsyncResponse : AsyncApiResponse<IReadOnlyList<UpcomingEarningsEvent>>
+{
+    public GetUpcomingEarningsResponse ToApiResp()
+    {
+        return ToApiResp<GetUpcomingEarningsResponse>();
+    }
+}
